49923d7cb691525905f71b37ecdaee00360cc153
jjas0nn <jason@kefi-foundation.org>
2019-08-25 19:01:35 -0700

    fix

--- surplus.py
+++ surplus.py
@@ -6,15 +6,6 @@ from bs4 import BeautifulSoup
 import numpy as np
 import urllib.request
 
-class Auction:
-    def __init__(self, number, title, time, cp):
-        self.auctionNum=number
-        self.auctionTitle=title
-        self.auctionTime=time
-        self.auctionCurrentPrice=float(cp)
-    def display(self):
-        return ("Auction Number: {} \nTitle: {} \nTime Remaining: {} \nCurrent Price: ${} \n".format(self.auctionNum, self.auctionTitle, self.auctionTime, self.auctionCurrentPrice))
-
 def load(url):
     src = urllib.request.urlopen(url).read()
     bs = BeautifulSoup(src, "html.parser")
