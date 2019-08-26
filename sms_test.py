322f543236b94387f6078d5e1f8cf6b7da139c20
jjas0nn <jason@kefi-foundation.org>
2019-08-26 01:26:21 -0700

    jj

--- sms_test.py
+++ sms_test.py
@@ -1,9 +1,10 @@
 from twilio.rest import Client
+import os
 
 # Your Account SID from twilio.com/console
-account_sid = ""
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # Your Auth Token from twilio.com/console
-auth_token  = ""
+auth_token  = os.environ['TWILIO_AUTH_TOKEN']
 my_num = "+12153302416"
 
 client = Client(account_sid, auth_token)
