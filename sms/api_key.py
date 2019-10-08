77bbd2d7fff1570df3c081f68c7848538d14cc5c
jason.jen <jj@jj-jj.org>
2019-10-08 01:55:25 -0700

    gitignore

--- sms/api_key.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['TWILIO_ACCOUNT_SID'] = 'ACf6519377bf2ea71fdfb027a4ac774240'
-os.environ['TWILIO_AUTH_TOKEN'] = '3ed7b245fe962eef8e137dc47c90755e'
-os.environ['ORIGIN_NUM'] = '+12153302416'
